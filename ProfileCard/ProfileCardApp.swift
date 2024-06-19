6a34c1b85e7d446293480a20039f80394b93d674
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-19 20:10:31 +0530

    Initial Commit

---
+++ ProfileCard/ProfileCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileCardApp.swift
+//  ProfileCard
+//
+//  Created by Yashwant Sheshkar on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ProfileCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
